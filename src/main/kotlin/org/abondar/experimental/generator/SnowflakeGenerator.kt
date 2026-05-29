1410a24d8fe5a326a5b8b46d300cafcc54e05725
Alex Bondar <abondar1992@gmail.com>
2026-05-29 12:44:06 +0200

    bugfixes

--- src/main/kotlin/org/abondar/experimental/generator/SnowflakeGenerator.kt
+++ src/main/kotlin/org/abondar/experimental/generator/SnowflakeGenerator.kt
@@ -19,7 +19,7 @@ class SnowflakeGenerator(
         private const val MAX_MACHINE_ID = (1L shl MACHINE_ID_BITS) - 1
         private const val MAX_SEQUENCE = (1L shl SEQUENCE_BITS) - 1
 
-        private const val TIMESTAMP_SHIFT =  SIGN_BITS + TIMESTAMP_BITS + MACHINE_ID_BITS
+        private const val TIMESTAMP_SHIFT =   MACHINE_ID_BITS + SEQUENCE_BITS
         private const val MACHINE_ID_SHIFT = SEQUENCE_BITS
         private const val EPOCH_START = "2024-01-01T00:00:00Z"
     }
@@ -45,25 +45,28 @@ class SnowflakeGenerator(
             throw IllegalStateException("Clock moved backwards. Generation is impossible")
         }
 
+        val seq: Long
         if (currentTimeStamp == lastTimestamp) {
-            val seq = (sequence.incrementAndGet() and MAX_SEQUENCE)
+            seq = (sequence.incrementAndGet() and MAX_SEQUENCE)
             if (seq == 0L) {
                 currentTimeStamp = waitNextMillis(currentTimeStamp) //wait for a new TS
+                sequence.set(0)
             }
         } else {
             sequence.set(0)
+            seq = 0L
         }
 
         lastTimestamp = currentTimeStamp
 
 
         return ((currentTimeStamp - epoch) shl TIMESTAMP_SHIFT) or
-                (machineId shl MACHINE_ID_SHIFT) or sequence.get()
+                (machineId shl MACHINE_ID_SHIFT) or seq
     }
 
     private fun waitNextMillis(currentTimeStamp: Long): Long {
         var timeStamp = currentTimeStamp
-        while (timeStamp < lastTimestamp){
+        while (timeStamp <= lastTimestamp){
             timeStamp = System.currentTimeMillis()
         }
 
